0ec1c0e18c24dd428e8dac557fd2cced5ebe2807
Simon Howroyd <howroydlsu@gmail.com>
2019-02-15 15:12:03 +0000

    more pretty graphs

--- LBORO/Vehicle Model/test.m
+++ LBORO/Vehicle Model/test.m
@@ -4,19 +4,24 @@ res = 0.25;
 
 set_param('cap_eq_circuit_pascal5_single_shot_stack','AlgebraicLoopSolver','LineSearch');
 
+distribution_in = [ 0 0 0 0 0 ...
+                    1 1 1 1 1 ...
+                    0 0 0 0 0];
+
+
 %% Initial conditions
 dt = 1.0;
-distribution_in = 0.0;
+%distribution_in = 0.0;
 amps_in = 0.0;
-[ v_end, amps_delivered, soc, distribution_out ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
+[ v_end, amps_delivered, soc, distribution_out, simout1 ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
 my_distribution = [ distribution_out ];
 v_cc = [0.0;v_end];
 
 %% Initial charge up
-dt = 20.0;
+dt = 100.0;
 distribution_in = distribution_out;
-amps_in = 2.0;
-[ v_end, amps_delivered, soc, distribution_out ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
+amps_in = 15.0;
+[ v_end, amps_delivered, soc, distribution_out, simout2 ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
 my_distribution = [my_distribution ; distribution_out];
 v_cc = [ v_cc ; v_end ];
 
@@ -24,7 +29,7 @@ v_cc = [ v_cc ; v_end ];
 dt = 10.0;
 distribution_in = distribution_out;
 amps_in = 0.0;
-[ v_end, amps_delivered, soc, distribution_out ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
+[ v_end, amps_delivered, soc, distribution_out, simout3 ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
 my_distribution = [my_distribution ; distribution_out];
 v_cc = [ v_cc ; v_end ];
 
@@ -32,7 +37,7 @@ v_cc = [ v_cc ; v_end ];
 dt = 5.0;
 distribution_in = distribution_out;
 amps_in = -0.05;
-[ v_end, amps_delivered, soc, distribution_out ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
+[ v_end, amps_delivered, soc, distribution_out, simout4 ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
 my_distribution = [my_distribution ; distribution_out];
 v_cc = [ v_cc ; v_end ];
 
@@ -40,7 +45,7 @@ v_cc = [ v_cc ; v_end ];
 dt = 500.0;
 distribution_in = distribution_out;
 amps_in = 0.0;
-[ v_end, amps_delivered, soc, distribution_out ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
+[ v_end, amps_delivered, soc, distribution_out, simout5 ] = sc_model_single_shot( dt, dt\res, amps_in, distribution_in );
 my_distribution = [my_distribution ; distribution_out];
 v_cc = [ v_cc ; v_end ];
 
@@ -69,6 +74,6 @@ zlabel('Voltage');
 %title('5th order pascal eq circuit, starting at 1V across each cap.  Apply load and see the fast states discharge.  Then open circuit and see the redistribution aka self balancing');
 hold off;
 
-figure();
-plot(v_cc);
-grid on;
+%figure();
+%plot(v_cc);
+%grid on;
